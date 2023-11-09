c62757b9712d89466be130f6908a0ffa43c2221a
Mark Julien Hahn <mail@markjulienhahn.de>
2023-11-09 10:40:43 +0100

    Press Error

--- components/Downloads.js
+++ components/Downloads.js
@@ -3,7 +3,7 @@ import styles from "../styles/Main.module.css";
 
 const Downloads = ({ english, downloads }) => {
   return (
-    <div style={{background: "var(--red)"}}>
+    <div style={{ background: "var(--red)" }}>
       {downloads.length != 0 ? (
         <>
           <div className={"anchor"} id="downloads"></div>
@@ -13,37 +13,36 @@ const Downloads = ({ english, downloads }) => {
             </div>
           </div>
 
-          <div
-            className={styles.downloadContentPresse}
-
-          >
-            {downloads.map((entry, i) => (
-              <div key={i} className={styles.downloadItem}>
-                <h1>
-                  {english ? entry.nameEN : entry.nameDE}
-                </h1>
-                <div>
-                  {entry.download.map((item, j) => (
-                    <a
-                      href={item.file.url}
-                      target="_blank"
-                      rel="noreferrer"
-                      key={j}
-                    >
-                      <div className={styles.downloadRow}>
-                        <p className={styles.fileExtension}>
-                          (↓)
-                          {/* ({item.file.extension}) */}
-                        </p>
-                        <p>
-                          {english ? item.beschreibungEN : item.beschreibungDE}
-                        </p>
-                      </div>
-                    </a>
-                  ))}
-                </div>
-              </div>
-            ))}
+          <div className={styles.downloadContentPresse}>
+            {downloads.length
+              ? downloads.map((entry, i) => (
+                  <div key={i} className={styles.downloadItem}>
+                    <h1>{english ? entry?.nameEN : entry?.nameDE}</h1>
+                    <div>
+                      {entry.download.map((item, j) => (
+                        <a
+                          href={item?.file?.url}
+                          target="_blank"
+                          rel="noreferrer"
+                          key={j}
+                        >
+                          <div className={styles.downloadRow}>
+                            <p className={styles.fileExtension}>
+                              (↓)
+                              {/* ({item.file.extension}) */}
+                            </p>
+                            <p>
+                              {english
+                                ? item?.beschreibungEN
+                                : item?.beschreibungDE}
+                            </p>
+                          </div>
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                ))
+              : ""}
           </div>
         </>
       ) : (
